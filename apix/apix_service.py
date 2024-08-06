5221b3d93264150f2c8813604d734d10bcb16720
PATRICK MORIN <patrick.morin@radio-canada.ca>
2024-08-06 15:22:50 -0400

    CHANGE how arguments to queries are handled.

--- apix/apix_service.py
+++ apix/apix_service.py
@@ -1,11 +1,14 @@
 """A module for querying Cisco's Service API end-points
 
-This module provides an interface for querying Cisco's inventory 
+This module provides an interface for querying Cisco's inventory
 hardware and network elements Service APIs.
 """
-from typing import Optional
-from typing import Dict
+from __future__ import annotations
+
 import time
+from typing import Dict
+from typing import Optional
+
 import httpx
 
 
@@ -33,6 +36,7 @@ class ApixService:
             'Accept': mime_type,
             'Authorization': auth_token,
         }
+        self.customerId: Optional[str] = None
         self.items = []
         self.records = []
 
@@ -53,21 +57,33 @@ class ApixService:
                 the API. Usually a 4xx client error or 5xx server error
                 response.
         """
-        with httpx.Client(timeout=10.0) as client:
-            response = client.get(url, 
-                                  headers=self.url_headers, 
-                                  params=params)
+        with httpx.Client(timeout=15.0) as client:
+            response = client.get(
+                url,
+                headers=self.url_headers,
+                params=params,
+            )
             response.raise_for_status()
             return response.json()
 
-    def query_hardware_inventory(self, customer_id: str, inventory_name: Optional[str] = None, hw_type: Optional[str] = None) -> None:
+    def query_hardware_inventory(self, **kwargs) -> None:
         """
         Query Cisco hardware inventory API.
 
         Args:
-            customer_id: Unique identifier of a Cisco customer (required).
-            inventoryName: Name of the inventory (optional).
-            hw_type: The physical type of the hardware (optional), such as Chassis, Module, etc.
+            This function accepts various keyword arguments that correspond
+            to query parameters in the API request. The customer ID is
+            automatically included from the instance's attribute. Other
+            accepted parameters can be included as keyword arguments, and they
+            will be directly passed to the API endpoint.
+
+            Examples of accepted keyword arguments include:
+            - inventoryName: Name of the inventory (optional).
+            - hwType: The physical type of the hardware (optional), such as Chassis, Module, etc.
+
+            service_api.query_hardware_inventory(inventoryName="some_inventory", hwType="Chassis")
+
+            Each keyword argument correlates with query parameters of the API request.
 
         Raises:
             httpx.HTTPStatusError: Raised if an HTTP error occurs when querying the API.
@@ -76,42 +92,75 @@ class ApixService:
 
         self.records = []   # Clear previous records before starting the new query
 
-        # Construct the query parameters
-        params = {'customerId': customer_id}
-        if inventory_name:
-            params['inventoryName'] = inventory_name
-        if hw_type:
-            params['hwType'] = hw_type
+        # Ensure 'customerId' key is present in kwargs
+        if 'customerId' not in kwargs:
+            kwargs['customerId'] = self.customerId
 
-        resp = self.__send_query(API_URL, params=params)
+        resp = self.__send_query(API_URL, params=kwargs)
         if resp.get('data'):
-                    self.records =  resp['data']
+            self.records = resp['data']
+            self.items = len(resp['data'])
         # Play nice with Cisco API's and rate limit your queries
         time.sleep(0.5)
 
+    def query_network_elements_inventory(self, **kwargs) -> None:
+        """
+        Query Cisco network elements inventory API.
 
-    def query_network_elements_inventory(self, customer_id: str, inventory_name: Optional[str] = None) -> None:
-            """
-            Query Cisco network elements inventory API.
+        Args:
+            This function accepts various keyword arguments that correspond
+            to query parameters in the API request. The customer ID is
+            automatically included from the instance's attribute. Other
+            accepted parameters can be included as keyword arguments, and they
+            will be directly passed to the API endpoint.
 
-            Args:
-                customer_id: Unique identifier of a Cisco customer (required).
-                inventoryName: Name of the inventory (optional).
+        Raises:
+            httpx.HTTPStatusError: Raised if an HTTP error occurs when querying the API.
+        """
 
-            Raises:
-                httpx.HTTPStatusError: Raised if an HTTP error occurs when querying the API.
-            """
-            API_URL = 'https://apix.cisco.com/cs/api/v1/inventory/network-elements'
+        API_URL = 'https://apix.cisco.com/cs/api/v1/inventory/network-elements'
 
-            self.records = []   # Clear previous records before starting the new query
+        self.records = []   # Clear previous records before starting the new query
 
-            # Construct the query parameters
-            params = {'customerId': customer_id}
-            if inventory_name:
-                params['inventoryName'] = inventory_name
+        # Ensure 'customerId' key is present in kwargs
+        if 'customerId' not in kwargs:
+            kwargs['customerId'] = self.customerId
 
-            resp = self.__send_query(API_URL, params=params)
-            if resp.get('data'):
-                        self.records =  resp['data']
-            # Play nice with Cisco API's and rate limit your queries
-            time.sleep(0.5)
+        resp = self.__send_query(API_URL, params=kwargs)
+        if resp.get('data'):
+            self.records = resp['data']
+            self.items = len(resp['data'])
+
+        # Play nice with Cisco API's and rate limit your queries
+        time.sleep(0.5)
+
+    def query_software_inventory(self, **kwargs) -> None:
+        """
+        Query Cisco software inventory API.
+
+        Args:
+            This function accepts various keyword arguments that correspond
+            to query parameters in the API request. The customer ID is
+            automatically included from the instance's attribute. Other
+            accepted parameters can be included as keyword arguments, and they
+            will be directly passed to the API endpoint.
+
+        Raises:
+            httpx.HTTPStatusError: Raised if an HTTP error occurs when querying the API.
+        """
+
+        API_URL = 'https://apix.cisco.com/cs/api/v1/inventory/software'
+
+        self.records = []   # Clear previous records before starting the new query
+
+        # Ensure 'customerId' key is present in kwargs
+        if 'customerId' not in kwargs:
+            kwargs['customerId'] = self.customerId
+
+        resp = self.__send_query(API_URL, params=kwargs)
+        if resp.get('data'):
+            self.records = resp['data']
+            self.items = len(resp['data'])
+
+        # Play nice with Cisco API's and rate limit your queries
+        time.sleep(0.5)
